e25aaa6a9dc0d24c791c0beab74431cfac0498d8
Mansard <nmansard@laas.fr>
2018-01-11 16:58:57 +0100

    Adding comments.

--- simu.py
+++ simu.py
@@ -5,18 +5,6 @@ from math import pi,sqrt,cos,sin
 from hrp2_reduced import Hrp2Reduced
 import time 
 
-pkg = '/home/nmansard/src/sot_versions/groovy/ros/stacks/hrp2/'
-urdf = '/home/nmansard/src/pinocchio/pinocchio/models/hrp2014.urdf'
-robot = Hrp2Reduced(urdf,[pkg],loadModel=True)
-robot.display(robot.q0)
-robot.viewer.setCameraTransform(0,[1.9154722690582275,
-                                   -0.2266872227191925,
-                                   0.1087859719991684,
-                                   0.5243823528289795,
-                                   0.518651008605957,
-                                   0.4620114266872406,
-                                   0.4925136864185333])
-
 def ForceVect(force_list):
     '''Convert a list of forces into a StdVect_Force'''
     res = se3.StdVect_Force()
@@ -29,38 +17,45 @@ def ForceDict(force_dict,N):
     res.extend(se3.Force.Zero() if i not in force_dict else force_dict[i] for i in range(N) )
     return res
 
-NQ = robot.model.nq
-NV = robot.model.nv
-NB = robot.model.nbodies
-RF = robot.model.getFrameId('rankle')
-LF = robot.model.getFrameId('lankle')
-RK = robot.model.frames[RF].parent
-LK = robot.model.frames[LF].parent
-
-
-# robot.viewer.addSphere('world/redball0', 0.05,[1.,.5,.5,1.])
-# robot.viewer.addBox('world/redsole0', 0.15,.06,.01,[1.,.5,.5,1.])
-# robot.viewer.applyConfiguration('world/redsole0',se3ToXYZQUAT(Mrf0))
-# robot.viewer.applyConfiguration('world/redball0',se3ToXYZQUAT(Mrf0))
-
 
 class Simu:
+    '''
+    Based on a Hrp2Reduced robot model, implement the simulation with spring
+    contact at the 2 ankles.
+    '''
+
     def __init__(self,robot,q0=None,dt=1e-3,ndt=10):
+        '''
+        Initialize from a Hrp2Reduced robot model, an initial configuration,
+        a timestep dt and a number of Eurler integration step ndt.
+        The <simu> method (later defined) processes <ndt> steps, each of them lasting <dt>/<ndt> seconds,
+        (i.e. total integration time when calling simu is <dt> seconds).
+        <q0> should be an attribute of robot if it is not given.
+        '''
         self.frf = zero(6)
         self.flf = zero(6)
         self.dt  = dt       # Time step
         self.ndt = ndt      # Discretization (number of calls of step per time step)
         self.robot = robot
 
+        self.NQ = robot.model.nq
+        self.NV = robot.model.nv
+        self.NB = robot.model.nbodies
+        self.RF = robot.model.getFrameId('rankle')
+        self.LF = robot.model.getFrameId('lankle')
+        self.RK = robot.model.frames[self.RF].parent
+        self.LK = robot.model.frames[self.LF].parent
+        NQ,NV,NB,RF,LF,RK,LK = self.NQ,self.NV,self.NB,self.RF,self.LF,self.RK,self.LK
+
         if q0 is None: q0 = robot.q0
         se3.forwardKinematics(robot.model,robot.data,q0)
         se3.framesKinematics(robot.model,robot.data)
 
-        self.Mrf0 = robot.data.oMf[RF].copy()
-        self.Mlf0 = robot.data.oMf[LF].copy()
+        self.Mrf0 = robot.data.oMf[RF].copy()  # Initial (i.e. 0-load) position of the R spring.
+        self.Mlf0 = robot.data.oMf[LF].copy()  # Initial (i.e. 0-load) position of the L spring.
 
-        self.Krf = -np.diagflat([20000.,200000.,00.])
-        self.Klf = -np.diagflat([20000.,200000.,00.])
+        self.Krf = -np.diagflat([20000.,200000.,00.])   # Stiffness of the Right spring
+        self.Klf = -np.diagflat([20000.,200000.,00.])   # Stiffness of the Left  spring
 
         robot.viewer.addXYZaxis('world/mrf',[1.,.6,.6,1.],.03,.1)
         robot.viewer.addXYZaxis('world/mlf',[.6,.6,1.,1.],.03,.1)
@@ -69,7 +64,9 @@ class Simu:
 
     def step(self,q,vq,tauq,dt = None):
         if dt is None: dt = self.dt
+
         robot = self.robot
+        NQ,NV,NB,RF,LF,RK,LK = self.NQ,self.NV,self.NB,self.RF,self.LF,self.RK,self.LK
 
         # --- Simu
         se3.forwardKinematics(robot.model,robot.data,q,v)
@@ -83,16 +80,16 @@ class Simu:
         qrf = np.vstack([Mrf.translation[1:],se3.rpy.matrixToRpy(Mrf.rotation)[0]])
         qlf = np.vstack([Mlf.translation[1:],se3.rpy.matrixToRpy(Mlf.rotation)[0]])
 
-        frf = self.frf
-        frf[[1,2,3]] = self.Krf*qrf
-        rf0_frf = se3.Force(frf) # Force in rf0 frame
-        rk_frf  = (robot.data.oMi[RK].inverse()*self.Mrf0).act(rf0_frf)  # Spring force in rk frame.
-        flf = self.flf
-        flf[[1,2,3]] = self.Klf*qlf
-        lf0_flf = se3.Force(flf) # Force in lf0 frame
-        lk_flf  = (robot.data.oMi[LK].inverse()*self.Mlf0).act(lf0_flf)  # Spring force in lk frame.
+        frf = self.frf                                                  # Buffer where to store right force
+        frf[[1,2,3]] = self.Krf*qrf                                     # Right force in effector frame
+        rf0_frf = se3.Force(frf)                                        # Force in rf0 frame
+        rk_frf  = (robot.data.oMi[RK].inverse()*self.Mrf0).act(rf0_frf) # Spring force in R-knee frame.
+        flf = self.flf                                                  # Buffer where to store left force
+        flf[[1,2,3]] = self.Klf*qlf                                     # Left force in effector frame
+        lf0_flf = se3.Force(flf)                                        # Force in lf0 frame
+        lk_flf  = (robot.data.oMi[LK].inverse()*self.Mlf0).act(lf0_flf) # Spring force in L-knee frame.
 
-        self.forces = ForceDict({ RK: rk_frf, LK: lk_flf},NB)
+        self.forces = ForceDict({ RK: rk_frf, LK: lk_flf},NB)           # Argument to ABA
 
         aq  = se3.aba(robot.model,robot.data,q,vq,tauq,self.forces)
         vq += aq*dt
@@ -102,6 +99,7 @@ class Simu:
         return q,vq
 
     def simu(self,q,v,tau):
+        '''Simu performs self.ndt steps each lasting self.dt/self.ndt seconds.'''
         for i in range(self.ndt):
             q,v = self.step(q,v,tau,self.dt/self.ndt)
         return q,v
@@ -110,6 +108,21 @@ class Simu:
 
 
 if __name__ == "__main__":
+    '''Simulation using a simple PD controller.'''
+
+
+    pkg = '/home/nmansard/src/sot_versions/groovy/ros/stacks/hrp2/'
+    urdf = '/home/nmansard/src/pinocchio/pinocchio/models/hrp2014.urdf'
+    robot = Hrp2Reduced(urdf,[pkg],loadModel=True)
+    robot.display(robot.q0)
+    robot.viewer.setCameraTransform(0,[1.9154722690582275,
+                                       -0.2266872227191925,
+                                       0.1087859719991684,
+                                       0.5243823528289795,
+                                       0.518651008605957,
+                                       0.4620114266872406,
+                                       0.4925136864185333])
+
     class ControlPD:
         def __init__(self,K):
             self.setGains(K)
@@ -138,21 +151,8 @@ if __name__ == "__main__":
         print 'Elapsed time = %.2f (simu time=%.2f)' % (time.time()-t0,simu.dt*niter)
         return q,v
 
-    def loopdg(q,v,niter,ndt=50,dt=5e-3,tsleep=.9,fdisplay=1):
-        t0 = time.time()
-        simu.dt  = dt/ndt
-        simu.ndt = 1
-        for i in range(niter):
-            if not i % fdisplay:
-                robot.display(q)
-                time.sleep(tsleep*dt)
-            for j in range(ndt):
-                q,v = simu(q,v,control(q,v))
-        print 'Elapsed time = %.2f (simu time=%.2f)' % (time.time()-t0,dt*niter)
-        return q,v
-
     q = robot.q0.copy()
-    v = zero(NV)
+    v = zero(simu.NV)
 
     q[0]-=.02
     q[1]-=.02
