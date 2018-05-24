dfe78837f07df6586cedd01fa704a233121a6c89
tflayols <tflayols@laas.fr>
2018-05-24 16:56:51 +0200

    Make rigid contact controller compatible with the flexible contact controller interface

--- tsid.py
+++ tsid.py
@@ -13,7 +13,7 @@ class Empty:
     pass
     
 class Tsid:
-    def __init__(self,robot,Kp_post,Kp_com):
+    def __init__(self,robot,Kp_post,Kp_com,w_post):
         self.robot = robot
         self.NQ = robot.model.nq
         self.NV = robot.model.nv
@@ -23,15 +23,18 @@ class Tsid:
         self.RK = robot.model.frames[self.RF].parent
         self.LK = robot.model.frames[self.LF].parent
         
+        self.w_post = w_post
         self.Kp_post = Kp_post
         self.Kd_post = 2*sqrt(Kp_post)
         self.Kp_com = Kp_com
         self.Kd_com = 2*sqrt(Kp_com)
         self.data = Empty()
-        com_p_ref = np.matrix([0.1,0.53]).T
-        com_v_ref = np.matrix([0,0]).T
-        com_a_ref = np.matrix([0,0]).T
-        self.callback_com = lambda t: (com_p_ref,com_v_ref,com_a_ref)
+        com_p_ref = np.matrix([0.,0.53]).T
+        com_v_ref = np.matrix([0.,0.]).T
+        com_a_ref = np.matrix([0.,0.]).T
+        com_j_ref = np.matrix([0.,0.]).T
+        com_s_ref = np.matrix([0.,0.]).T
+        self.callback_com = lambda t: (com_p_ref,com_v_ref,com_a_ref,com_j_ref,com_s_ref )
     def setGains(self,P,V=None):
         if V is None: 
             V=2*sqrt(P)
@@ -39,47 +42,34 @@ class Tsid:
     def solve(self,q,v,t=0.0):
         robot=self.robot
         NQ,NV,NB,RF,LF,RK,LK = self.NQ,self.NV,self.NB,self.RF,self.LF,self.RK,self.LK
-        
+        w_post = self.w_post
         Kp_post, Kd_post = self.Kp_post, self.Kd_post
         Kp_com,  Kd_com  = self.Kp_com,  self.Kd_com
         callback_com = self.callback_com
-        #~ print("\033c") # clear terminal
-        # compute and extract quantities *******************************
+
         se3.computeAllTerms(robot.model,robot.data,q,v)
         se3.framesKinematics(robot.model,robot.data,q)
-        #~ se3.rnea(robot.model,robot.data,q,v,0*v)
+        se3.rnea(robot.model,robot.data,q,v,0*v)
         M  = robot.data.M        #(7,7)
         h  = robot.data.nle      #(7,1)
-        Jl = se3.frameJacobian(robot.model,robot.data,LF)       
-        Jr = se3.frameJacobian(robot.model,robot.data,RF)
-        Jlk = se3.jacobian(robot.model,robot.data,q,LK,True,True)
-        Jrk = se3.jacobian(robot.model,robot.data,q,RK,True,True)
+        
+        Jl,Jr = robot.get_Jl_Jr_world(q, False)
 
         # Formulate contact and dynamic constrains *********************
         #        7    4     4       7  4  4
         # 6   |Jc    0      0 | * [dv,f,tau].T =  |-dJc*dq|
         # 7   |M   -Jc.T  -S.T|                   |  -h   |
 
-        Jk = np.vstack([Jlk[1:3],Jrk[1:3]])  # (4, 7)
         Jc = np.vstack([Jl[1:3],Jr[1:3]])    # (4, 7)
         S  = np.hstack([np.zeros([4,3]),np.eye(4)]) # (4,7)
         z1 = np.matrix(np.zeros([4,4]))
         z2 = np.matrix(np.zeros([4,4]))
 
-        #~ driftLF, driftRF = robot.data.a[LF] , robot.data.a[RF]
-        #~ v_LF,    v_RF    = robot.data.v[LF] , robot.data.v[RF]
-        v_LF = robot.model.frames[LF].placement.actInv(robot.data.v[robot.model.frames[LF].parent]).copy()
-        a_LF = robot.model.frames[LF].placement.actInv(robot.data.a[robot.model.frames[LF].parent]).copy()
-        v_RF = robot.model.frames[RF].placement.actInv(robot.data.v[robot.model.frames[RF].parent]).copy()
-        a_RF = robot.model.frames[RF].placement.actInv(robot.data.a[robot.model.frames[RF].parent]).copy()
-        driftLF = a_LF
-        driftRF = a_RF
-        driftLF.linear += np.cross(v_LF.angular.T, v_LF.linear.T).T
-        driftRF.linear += np.cross(v_RF.angular.T, v_RF.linear.T).T
+        driftLF,driftRF = robot.get_driftLF_driftRF_world(q,v, False)
         dJcdq = np.vstack([driftLF.vector[1:3],driftRF.vector[1:3]])
 
         Aec = np.vstack([ np.hstack([Jc, z1  ,z2])  ,
-                          np.hstack([M ,-Jc.T,-S.T])  ])  # -Jk.T
+                          np.hstack([M ,-Jc.T,-S.T])  ]) 
         bec = np.vstack([-dJcdq,-h])
         #only contacts
         #~ Ac = np.hstack([Jc, z1  ,z2]) 
@@ -110,13 +100,17 @@ class Tsid:
         #CoM task ******************************************************
         Jcom = robot.data.Jcom[1:] #Only Y and Z
 
-        com_p_ref, com_v_ref, com_a_ref = callback_com(t)
+        com_p_ref, com_v_ref, com_a_ref, com_j_ref, com_s_ref = callback_com(t)
         se3.centerOfMass(robot.model,robot.data,q,v,zero(NV))
-        com  = robot.data.com[0][1:]
-        vcom = robot.data.vcom[0][1:]
+        com_mes   = robot.data.com[0][1:] 
+        com_v_mes = robot.data.vcom[0][1:]
+        
+        com_est   = com_mes#Todo use an estimator
+        com_v_est = com_v_mes
+        
         com_drift = robot.data.acom[0][1:]
-        com_p_err =  com - com_p_ref
-        com_v_err = vcom - com_v_ref
+        com_p_err =   com_est - com_p_ref
+        com_v_err = com_v_est - com_v_ref
 
         com_a_des = -Kp_com*com_p_err -Kd_com*com_v_err +com_a_ref
 
@@ -124,10 +118,8 @@ class Tsid:
 
         A_com  = np.hstack([Jcom,z3])
         b_com  = com_a_des - com_drift
-        #~ if not log_index%100 :
-            #~ print "com error \t{0}".format(np.linalg.norm(com_p_err))
-        #posture task  *************************************************
 
+        #posture task  *************************************************
         Jpost = np.hstack([np.zeros([4,3]),np.eye(4)])
         post_p_ref = robot.q0[4:] #only the actuated part !
         post_v_ref = np.matrix([0,0,0,0]).T
@@ -135,21 +127,27 @@ class Tsid:
 
         post_p_err = q[4:] - post_p_ref
         post_v_err = v[3:] - post_v_ref
-
-        post_a_des = -Kp_post*post_p_err - Kd_post*post_v_err #No a_ref here
+        
+        post_a_des = -Kp_post*post_p_err - Kd_post*post_v_err 
 
         z4 = np.matrix(np.zeros([4,4+4]))
-        A_post  = np.hstack([Jpost,z4])
-        b_post  = post_a_des # -post_drift
+        A_post  = w_post*np.hstack([Jpost,z4])
+        b_post  = w_post*post_a_des # -post_drift
         #~ print "posture error \t{0}".format(np.linalg.norm(post_p_err))
 
         #stack all tasks
         A=np.vstack([A_com,A_post])
         b=np.vstack([b_com,b_post])
-        
+        #~ A=A_post
+        #~ b=b_post
+        #~ A=A_com
+        #~ b=b_com
+
         #stack equality and inequality constrains
-        Ac = np.vstack([Aec,Aic])
-        bc = np.vstack([bec,bic])
+        #~ Ac = np.vstack([Aec,Aic])
+        #~ bc = np.vstack([bec,bic])
+        Ac=Aec
+        bc=bec
         
         #formulate the least square as a quadratic problem *************
         H=(A.T*A).T + 1e-10*np.eye(A.shape[1])
@@ -168,11 +166,38 @@ class Tsid:
         dv = np.matrix(y[:7]   ).T
         f   = np.matrix(y[7:7+4]).T
         tau = np.matrix(y[7+4:] ).T
+        
+        
         #populate results
-        self.data.com    = com.A1
+        self.data.lf_a_des = np.matrix([0.,0.]).T
+        self.data.rf_a_des = np.matrix([0.,0.]).T
+        self.data.com_p_mes  = com_mes.A1
+        self.data.com_v_mes  = com_v_mes.A1
+        self.data.com_a_mes  = np.matrix([0.,0.]).T.A1
+        #~ self.data.com_j_mes  = com_j_mes.A1 # should not be able to measure jerk
+        
+        self.data.com_p_est  = com_est.A1
+        self.data.com_v_est  = com_v_est.A1
+        self.data.com_a_est  = np.matrix([0.,0.]).T.A1
+        self.data.com_j_est  = np.matrix([0.,0.]).T.A1
+        
+        self.data.com_a_des = com_a_des.A1
+        self.data.com_s_des = np.matrix([0.,0.]).T.A1 #None in rigid contact controller
+        
         self.data.com_p_err = com_p_err.A1
         self.data.com_v_err = com_v_err.A1
+        self.data.com_a_err = np.matrix([0.,0.]).T.A1
+        self.data.com_j_err = np.matrix([0.,0.]).T.A1
         self.data.comref = com_p_ref.A1
+        
+        self.data.robotInertia = 0
+
+        self.data.iam  = 0
+        self.data.am   = 0
+        self.data.dam  = 0
+        self.data.ddam = 0
+        self.data.dddam_des = 0
+        
         self.data.lkf    = f[:2].A1
         self.data.rkf    = f[2:].A1
         self.data.tau    = tau
@@ -189,7 +214,7 @@ if __name__ == "__main__":
     import time
     from path import pkg, urdf 
     robot = Hrp2Reduced(urdf,[pkg],loadModel=True,useViewer=False)
-    tsid=Tsid(robot,Kp_post=10,Kp_com=30)
+    tsid=Tsid(robot,Kp_post=10,Kp_com=30,w_post=0.001)
     niter= 3000
     t0 = time.time()
     for i in range(niter):
