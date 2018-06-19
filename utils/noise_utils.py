cfba9cbcb459e90dab05aa8b3379db956b8a68fe
Andrea del Prete <adelprete@tuebingen.mpg.de>
2018-06-19 12:25:32 +0200

    Remove useless import

--- utils/noise_utils.py
+++ utils/noise_utils.py
@@ -1,9 +1,5 @@
-import pinocchio as se3
-from pinocchio.robot_wrapper import RobotWrapper
 from pinocchio.utils import *
-from math import pi,sqrt
 import matplotlib.pyplot as plt
-from filters import FiniteDiff
 
 try:
     from IPython import embed
