200bec0ac595dfad02a88a9e5463643da3d7e69e
Andrea del Prete <adelprete@tuebingen.mpg.de>
2018-11-30 18:44:02 +0100

    Add function to plot data contained in multikey dictionary

--- utils/regex_dict.py
+++ utils/regex_dict.py
@@ -6,6 +6,9 @@ Created on Tue Nov 27 14:35:34 2018
 """
 
 import re
+import numpy as np
+import matplotlib.pyplot as plt
+import simple_biped.utils.plot_utils as plut
 
 class RegexDict(dict):
     
@@ -35,6 +38,35 @@ class RegexDict(dict):
         matching_keys = self.get_matching_keys(keys, values)
         return (self[key] for key in matching_keys)
 
+
+def plot_from_multikey_dict(d, keys, x_var, y_var, fixed_params, variab_param, xscale=None, yscale=None):
+    matching_keys = d.get_matching_keys(keys, fixed_params)
+    x, y = {}, {}
+    for mk in matching_keys:
+        z_i = d.extract_key_value(mk, variab_param)
+        if z_i not in x: x[z_i], y[z_i] = [], []    
+        data = d[mk]
+        x[z_i] += [float(d.extract_key_value(mk, x_var))]
+        y[z_i] += [data.__dict__[y_var]]
+    
+    plt.figure()
+    for key in x.keys():
+        xy = np.array(zip(x[key], y[key]), dtype=[('x', float), ('y', float)])
+        xy_sorted = np.sort(xy, order='x')
+        x_sorted = [xi for (xi,yi) in xy_sorted]
+        y_sorted = [yi for (xi,yi) in xy_sorted]
+        plt.plot(x_sorted, y_sorted, '-*', label=key)
+    plt.xlabel(x_var)
+    plt.ylabel(y_var)
+    plt.legend()
+    if xscale is not None: plt.xscale(xscale)
+    if yscale is not None: plt.yscale(yscale)
+    fig_name = y_var+'_VS_'+x_var
+    for (k,v) in zip(keys, fixed_params):
+        if(v is not None):
+            fig_name += '__'+k+'='+str(v)
+    plut.saveFigure(fig_name)
+    return (x,y)
 
 if __name__ == '__main__':
     keys = ['ctrl', 'zeta', 'k']
