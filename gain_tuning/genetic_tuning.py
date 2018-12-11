8abd18a5101930f5f3cb7747e994bd79a41f3d24
Andrea del Prete <adelprete@tuebingen.mpg.de>
2018-12-11 16:42:28 +0100

    Switch from cost function penalizing forces to cost function penalizing only CoM and its derivatives, to be consistent with TSID-Adm gain tuning.

--- gain_tuning/genetic_tuning.py
+++ gain_tuning/genetic_tuning.py
@@ -11,6 +11,7 @@ import numpy as np
 from numpy.linalg import norm, eigvals
 from scipy.linalg import expm
 from scipy.optimize import basinhopping
+from scipy.linalg import block_diag
 import matplotlib.pyplot as plt
 
 from pinocchio.utils import *
@@ -31,17 +32,19 @@ class GainOptimizer(object):
     
     def __init__(self):
         self.nit = 0
+        self.initial_cost = 1.0
     
     def callback(self, x, f, accept):
-        print "%4d) Cost: %10f; Accept %d Gains:"%(self.nit, f, accept), x
+        print "%4d) Cost: %5f; Accept %d Gains:"%(self.nit, f/self.initial_cost, accept), x
         self.nit += 1;
 
     def optimize_gains(self, gains, cost_function, niter=100, verbose=0):
         ''' Optimize the gains of a controller based on the cost function
         '''
-        if(verbose):
+        self.initial_cost = cost_function(gains)
+        if(verbose):            
             print "Initial gains:   ", gains.T
-            print "Initial cost     ", cost_function(gains)
+            print "Initial cost     ", self.initial_cost
         self.nit = 0
         opt_res = basinhopping(cost_function, gains, niter, disp=False, T=0.1, stepsize=.01, callback=self.callback)
         opt_gains = opt_res.x
@@ -76,6 +79,8 @@ class GainOptimizeAdmCtrl(GainOptimizer):
         
         self.update_state(q, v)
         
+        self.nominal_cost = self.cost_function(np.ones_like(nominal_gains))
+        
     def set_cost_function_matrix(self, Q):
         self.Q = Q
     
@@ -99,7 +104,8 @@ class GainOptimizeAdmCtrl(GainOptimizer):
         com, com_vel = self.robot.get_com_and_derivatives(q, v)
         cy, cz     = com.A1
         X_am  = np.matrix([-(pzl-cz),+(pyl-cy),-(pzr-cz),+(pyr-cy)])
-        X_com = np.hstack([np.eye(2)/M[0,0],np.eye(2)/M[0,0]])
+#        X_com = np.hstack([np.eye(2)/M[0,0],np.eye(2)/M[0,0]])
+        X_com = np.hstack([np.eye(2),np.eye(2)])
         X = np.vstack([X_com, X_am])
         self.X_pinv = np.linalg.pinv(X)
         
@@ -138,10 +144,11 @@ class GainOptimizeAdmCtrl(GainOptimizer):
         not_finite_warning_printed = 1
         for i in range(self.N):
             if(np.all(np.isfinite(x[:,i]))):
-                cost += self.dt*(x[:,i].T * self.Q * x[:,i])[0,0]
+                last_finite_x = x[:,i]
             elif(not not_finite_warning_printed):
                 print 'WARNING: x is not finite at time step %d'%(i) #, x[:,i].T
                 not_finite_warning_printed = True
+            cost += self.dt*(last_finite_x.T * self.Q * last_finite_x)[0,0]
         cost /= (self.N*self.dt)
 #        print 'Gains:', GainsAdmCtrl(gains_array).to_string()
 #        print "Largest eigenvalues:", np.sort_complex(eigvals(H))[-4:].T,
@@ -151,15 +158,35 @@ class GainOptimizeAdmCtrl(GainOptimizer):
 #            step_response(H, self.x0, self.dt, self.N, 1)
         return cost
         
+    def normalized_cost_function(self, normalized_gains):
+        cost = self.cost_function(normalized_gains)
+        return cost/self.nominal_cost
+        
     def optimize_gains(self, niter=100):
         initial_guess = np.ones_like(self.nominal_gains)
         
-        opt_gains_normalized = super(GainOptimizeAdmCtrl, self).optimize_gains(initial_guess, self.cost_function, niter)
+        opt_gains_normalized = super(GainOptimizeAdmCtrl, self).optimize_gains(initial_guess, self.normalized_cost_function, niter)
         opt_gains = self.denormalize_gains_array(opt_gains_normalized)
         
         return opt_gains
+
+def convert_cost_function(w_x, w_dx, w_d2x, w_d3x, w_d4x):
+    ''' Convert the cost function of the CoM state in the cost function
+        of the momentum+force state.
+    '''
+    nl = 2
+    ny = 3
+#    robot   = Hrp2Reduced(urdf, [pkg], loadModel=0, useViewer=0)
+    m       = 1.0 #np.sum([robot.model.inertias[i].mass for i in range(1,len(robot.model.inertias))])
+    Q_diag  = np.matrix(nl*[w_x] + nl*[w_dx] + nl*[w_d2x] + nl*[w_d3x] + nl*[w_d4x])
+    Q_c     = matlib.diagflat(Q_diag)                           # weight matrix for CoM state
+    S       = np.hstack((np.eye(nl), np.zeros((nl,ny-nl))))     # matrix selecting CoM from x
+    SA      = np.hstack((np.eye(nl), np.eye(nl)))               # matrix adding forces
+    P       = np.matrix((1.0/m)*block_diag(S, S, SA, SA, SA))   # projection from full state to CoM state
+    Q       = P.T * Q_c * P
+    return Q
     
-def optimize_gains_adm_ctrl(w_x, w_dx, w_f, w_df, w_ddf, N, dt, max_iter, x0=None, initial_guess=None, do_plots=0):
+def optimize_gains_adm_ctrl(Q, N, dt, max_iter, x0=None, initial_guess=None, do_plots=0):
     # User parameters
     PROFILE_ON = 0
     ny = 3
@@ -174,8 +201,6 @@ def optimize_gains_adm_ctrl(w_x, w_dx, w_f, w_df, w_ddf, N, dt, max_iter, x0=Non
     q       = robot.q0.copy()
     v       = zero(robot.model.nv)
     K       = Simu.get_default_contact_stiffness()
-    Q_diag  = np.matrix(ny*[w_x] + ny*[w_dx] + nf*[w_f] + nf*[w_df] + nf*[w_ddf])
-    Q       = matlib.diagflat(Q_diag)
     
     if(initial_guess is None):
         initial_guess   = GainsAdmCtrl.get_default_gains(K).to_array()
@@ -201,17 +226,22 @@ def optimize_gains_adm_ctrl(w_x, w_dx, w_f, w_df, w_ddf, N, dt, max_iter, x0=Non
 
 
 if __name__=='__main__':
-    N       = 400
+    N       = 1000
     dt      = 1e-2
-    w_x     = 1.0
-    w_dx    = 0.0
-    w_f     = 0.0
-    w_df    = 0.0
-    w_ddf   = 1e-7
+    w_x         = 1.0
+    w_dx        = 1e-1 #0.0
+    w_d2x       = 1e-3
+    w_d3x       = 1e-6
+    w_d4x       = 1e-9
     max_iter = 2
     do_plots = 1
+    ny = 3
+    nf = 4
+    x0          = matlib.zeros((3*nf+2*ny,1))
+    x0[ny,0]    = .0765     # initial CoM velocity in Y direction
     
-    optimal_gains = optimize_gains_adm_ctrl(w_x, w_dx, w_f, w_df, w_ddf, N, dt, max_iter, do_plots)
+    Q = convert_cost_function(w_x, w_dx, w_d2x, w_d3x, w_d4x)
+    optimal_gains = optimize_gains_adm_ctrl(Q, N, dt, max_iter, x0=x0, do_plots=do_plots)
     
     print "Initial gains:\n", GainsAdmCtrl.get_default_gains(Simu.get_default_contact_stiffness()).to_string()
     print "Optimal gains:\n", GainsAdmCtrl(optimal_gains).to_string()
