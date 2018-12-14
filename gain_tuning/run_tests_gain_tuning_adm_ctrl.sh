50de6ea8367bc304bc4bb7240592d48b3c250b00
Andrea del Prete <adelprete@tuebingen.mpg.de>
2018-12-14 19:16:18 +0100

    Add scripts to tune gains and run tests

---
+++ gain_tuning/run_tests_gain_tuning_adm_ctrl.sh
@@ -0,0 +1,3 @@
+python tune_gains_adm_ctrl.py
+python run_tests_adm_ctrl.py 
+python -i analyze_gain_tuning_adm_ctrl.py
---
+++ gain_tuning/run_tests_gain_tuning_tsid_adm.sh
@@ -0,0 +1,3 @@
+#python tune_gains_tsid_adm.py
+python run_tests_tsid_adm.py 
+python -i analyze_gain_tuning_tsid_adm.py
