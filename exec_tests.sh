b91d89c229dd8b2e768c8705ec11fe79a2d60faa
Andrea del Prete <adelprete@tuebingen.mpg.de>
2018-12-14 19:15:48 +0100

    Add script to execute some tests

---
+++ exec_tests.sh
@@ -0,0 +1,27 @@
+python track_com.py --controller=tsid_flex   --com_sin_amp=0.03 --f_dist=0   --zeta=0.3  --T=2.0
+python track_com.py --controller=tsid_rigid  --com_sin_amp=0.03 --f_dist=0   --zeta=0.3  --T=2.0
+python track_com.py --controller=tsid_adm    --com_sin_amp=0.03 --f_dist=0   --zeta=0.3  --T=2.0
+python track_com.py --controller=tsid_mistry --com_sin_amp=0.03 --f_dist=0   --zeta=0.3  --T=2.0
+python track_com.py --controller=adm_ctrl    --com_sin_amp=0.03 --f_dist=0   --zeta=0.3  --T=2.0
+
+python track_com.py --controller=tsid_flex   --com_sin_amp=0.0  --f_dist=400   --zeta=0.3  --T=1.5
+python track_com.py --controller=tsid_rigid  --com_sin_amp=0.0  --f_dist=400   --zeta=0.3  --T=1.5
+python track_com.py --controller=tsid_adm    --com_sin_amp=0.0  --f_dist=400   --zeta=0.3  --T=1.5
+python track_com.py --controller=tsid_mistry --com_sin_amp=0.0  --f_dist=400   --zeta=0.3  --T=1.5
+python track_com.py --controller=adm_ctrl    --com_sin_amp=0.0  --f_dist=400   --zeta=0.3  --T=1.5
+
+python track_com.py --controller=tsid_flex   --com_sin_amp=0.0 --f_dist=400   --zeta=0.9  --T=1.5
+python track_com.py --controller=tsid_rigid  --com_sin_amp=0.0 --f_dist=400   --zeta=0.9  --T=1.5
+python track_com.py --controller=tsid_adm    --com_sin_amp=0.0 --f_dist=400   --zeta=0.9  --T=1.5
+python track_com.py --controller=tsid_mistry --com_sin_amp=0.0 --f_dist=400   --zeta=0.9  --T=1.5
+python track_com.py --controller=adm_ctrl    --com_sin_amp=0.0 --f_dist=400   --zeta=0.9  --T=1.5
+
+python track_com.py --controller=tsid_flex   --com_sin_amp=0.0 --f_dist=400   --zeta=0.2  --T=1.5
+python track_com.py --controller=tsid_rigid  --com_sin_amp=0.0 --f_dist=400   --zeta=0.2  --T=1.5
+python track_com.py --controller=tsid_adm    --com_sin_amp=0.0 --f_dist=400   --zeta=0.2  --T=1.5
+python track_com.py --controller=adm_ctrl    --com_sin_amp=0.0 --f_dist=400   --zeta=0.2  --T=1.5
+
+python track_com.py --controller=tsid_flex   --com_sin_amp=0.0 --f_dist=400   --zeta=0.1  --T=1.5
+python track_com.py --controller=tsid_rigid  --com_sin_amp=0.0 --f_dist=400   --zeta=0.1  --T=1.5
+python track_com.py --controller=tsid_adm    --com_sin_amp=0.0 --f_dist=400   --zeta=0.1  --T=1.5
+python track_com.py --controller=adm_ctrl    --com_sin_amp=0.0 --f_dist=400   --zeta=0.1  --T=1.5
