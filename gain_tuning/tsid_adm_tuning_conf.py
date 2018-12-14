2b5d969caad5f887118bd02b89dbc2d0883e6339
Andrea del Prete <adelprete@tuebingen.mpg.de>
2018-12-14 19:13:57 +0100

    Update config of gain tuning of TSID-Adm to account that initial state is already perturbed

--- gain_tuning/tsid_adm_tuning_conf.py
+++ gain_tuning/tsid_adm_tuning_conf.py
@@ -22,33 +22,33 @@ def get_gains_file_name(w_d4x):
     
 DATA_DIR                = str(os.path.dirname(os.path.abspath(__file__)))+'/../data/'
 GAINS_DIR_NAME          = 'gains/'
-TESTS_DIR_NAME          = 'test_gain_tuning/tsid_amd/'
+TESTS_DIR_NAME          = 'test_gain_tuning/tsid_amd_w_dx_0.1_d2x_1e-3_d3x_1e-6/'
 GAINS_FILE_NAME         = 'gains_tsid_adm'
 
 keys                = ['ctrl', 'fDist', 'zeta', 'w_d4x']
 controllers         = ['tsid_adm']
-f_dists             = [400.]
+f_dists             = [0.]
 zetas               = [0.3]
 T_cost_function     = 10.0
 T_simu              = 5.0
 dt_cost_function    = 1e-2  # time step used for cost function of gain tuning
 dt_simu             = 1e-3  # time step used in simulations
 mu                  = 0.3
-T_DISTURB_BEGIN     = 0.11
+T_DISTURB_BEGIN     = 0.0
 
 w_x         = 1.0
 w_dx        = 1e-1 #0.0
 w_d2x       = 1e-3
 w_d3x       = 1e-6
-w_d4x_list  = np.logspace(-12.0, -7.0, num=6)
+w_d4x_list  = np.logspace(-12.0, -6.0, num=7)
 #w_d4x_list  = np.hstack((np.logspace(-10.0, -7.0, num=4)) , [2.5, 5.0]))
 
 ny          = 1
 nf          = 4
 x0          = matlib.zeros((4*ny,1))
-x0[1,0]    = .0765     # initial CoM velocity in Y direction
+x0[1,0]    = .1     # initial CoM velocity in Y direction
 
-do_plots    = 1
+do_plots    = 0
 
 Q_pos       = matlib.diagflat(np.matrix(ny*[w_x] + ny*[w_dx] + ny*[w_d2x] + ny*[w_d3x]))
 Q_d4x       = matlib.diagflat(np.matrix(ny*[0.0] + ny*[0.] + ny*[0.] + ny*[0.]))
